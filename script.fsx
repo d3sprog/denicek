e5122ce63e509012e81e0059afa025a8bfb78d50
Tomas Petricek <tomas@tomasp.net>
2023-04-02 00:53:18 +0200

    Simpler HTML based version

--- script.fsx
+++ script.fsx
@@ -1,111 +1,188 @@
-type Element = 
-  | Text of string
-  | Emphasis of Elements
-  | Paragraph of Elements
-  | Heading of int * Elements
-  | List of Elements list
-  | Table of string list * Elements list
+// --------------------------------------------------------------------------------------
+// Elements, Selectors, Paths
+// --------------------------------------------------------------------------------------
 
+type Element = 
+  | Element of Attributes * Elements
+  | Text of string
+
+and Attributes = Map<string, string>
 and Elements = Element list
 
-
-type Selector =
-  | Nth of int list
-  | Children
-
+type Selector = Map<string, string> -> bool
 type Selectors = Selector list
 
-let rec select sel els = 
-  match sel with 
-  | [] -> els
-  | Nth(is)::sel ->
-      select sel [ for i in is -> els.[i] ] 
-  | Children::sel ->
-      [ for el in els do 
-          match el with 
-          | Emphasis els | Paragraph els | Heading(_, els) -> yield! els
-          | List(elss) | Table(_, elss) -> yield! Seq.concat elss
-          | _ -> () ] |> select sel
+let Any : Selector = fun _ -> true
+let Tag t : Selector = fun m -> m.["tag"] = t
+let Index (i:int) : Selector = fun m -> m.["index"] = string i
 
-let rec replace sel repl (els:_ list) = 
-  match sel with 
-  | [] -> repl
-  | Nth(is)::sel ->
-      let repls = replace sel repl [ for i in is -> els.[i] ]
-      [ for i, el in Seq.indexed els ->
-          if List.contains i is then 
-            let j = List.findIndex ((=) i) is
-            repls.[j]
-          else el ]
-  | Children::sel ->
-      [ for el in els do 
-          match el with 
-          | Emphasis els -> Emphasis(replace sel repl els)
-          | Paragraph els -> Paragraph(replace sel repl els)
-          | Heading(n, els) -> Heading(n, replace sel repl els)
-          | List(elss) -> List([for els in elss -> replace sel repl els])
-          | Table(cols, elss) -> Table(cols,[for els in elss -> replace sel repl els])
-          | el -> el ]
+type Path = 
+  | Child of Map<string, string>
+type Paths = Path list
+
+// --------------------------------------------------------------------------------------
+// Matching, folds, replaces
+// --------------------------------------------------------------------------------------
+
+let rec matchesText el (path:Paths) (sel:Selectors) = 
+  match el with 
+  | Text _ -> 
+      let path = path |> List.rev
+      List.length path = List.length sel &&
+        (List.forall2 (fun (Child(attrs)) f -> f attrs) path sel)
+  | _ -> false
+
+let rec matches el (path:Paths) (sel:Selectors) = 
+  match el with 
+  | Element(attrs, _) -> 
+      let path = Child(attrs)::path |> List.rev
+      List.length path = List.length sel &&
+        (List.forall2 (fun (Child(attrs)) f -> f attrs) path sel)
+  | _ -> false
+
+let fold f st el = 
+  let rec loop path st el =
+    let st = f path el st 
+    match el with 
+    | Element(attrs, children) -> 
+        children |> List.fold (loop (Child(attrs)::path)) st
+    | Text s -> 
+        st
+  loop [] st el      
+
+let replace f el = 
+  let rec loop path el =
+    match f path el with 
+    | Some res -> res
+    | _ -> 
+    match el with 
+    | Element(attrs, children) -> 
+        Element(attrs, List.map (loop (Child(attrs)::path)) children)
+    | Text s -> 
+        Text s
+  loop [] el      
+
+let select sel doc = 
+  doc |> fold (fun p el st ->
+    if matches el p sel then el::st else st) [] 
+
+// --------------------------------------------------------------------------------------
+// Edits
+// --------------------------------------------------------------------------------------
 
 type Edit =
-  | EditElement of Selectors * (Element -> Element)
-  | MakeTable of Selectors * Selectors
-  | SplitColumn of Selectors * string * string list * (Element -> Element list)
+  | SetAttr of Selectors * (string * string)
+  | Nest of Selectors * (Elements -> Element)
+  | EditText of Selectors * (string -> string)
+  | Duplicate of Selectors
 
-let apply edit els = 
-  match edit with
-  | EditElement(elementSel, update) ->
-      replace elementSel (List.map update (select elementSel els)) els
-  | MakeTable(replaceSel, child) ->
-      let repl = Table(["Column"], List.map List.singleton (select child els))
-      replace replaceSel [repl] els
-  | SplitColumn(tableSel, col, newCols, splitter) ->
-      match select tableSel els with
-      | [Table(cols, rows)] ->
-          let i = List.tryFindIndex ((=) col) cols // NOTE: Does not handle duplicates
-          let i = match i with Some i -> i | _ -> failwithf "apply.SplitColumn: Column '%s' not found" col
-          let ncols = cols |> List.collect (fun c -> if c = col then newCols else [c])
-          let nrows = rows |> List.map (fun row -> 
-            List.indexed row |> List.collect (fun (j, col) -> 
-              if i = j then splitter col else [col]))
-          replace tableSel [Table(ncols, nrows)] els
-      | _ -> failwith "apply.SplitColumn: Expected single table"
-      
+let updateIndices el = 
+  let rec loop i = function
+    | Element(attrs, children) ->
+        Element(attrs.Add("index", string i), List.mapi loop children)
+    | el -> el
+  loop 0 el 
+
+let apply edit doc = 
+  updateIndices <|
+    match edit with 
+    | Duplicate(sel) ->
+        let last, sel = match List.rev sel with last::sel -> last, List.rev sel | _ -> failwith "apply.Duplicate: Needs selector"
+        replace (fun p el -> 
+          match el with 
+          | Element(attrs, children) when matches el p sel -> 
+              let nchildren = children |> List.collect (function 
+                | Element(attrs, c) as el when last attrs -> [el; el]
+                | el -> [el])
+              Some(Element(attrs, nchildren))
+          | _ -> None ) doc  
+    | EditText(sel, f) -> 
+        replace (fun p el -> 
+          match el with 
+          | Text(s) when matchesText el p sel -> Some(Text(f s))
+          | _ -> None ) doc
+    | Nest(sel, wrapper) -> 
+        replace (fun p el -> 
+          match el with 
+          | Element(attrs, children) when matches el p sel -> 
+              Some(wrapper [el])
+          | _ -> None ) doc
+    | SetAttr(sel, (k, v)) -> 
+        replace (fun p el -> 
+          match el with 
+          | Element(attrs, children) when matches el p sel -> 
+              Some(Element(attrs.Add(k, v), children))
+          | _ -> None ) doc
+
+// --------------------------------------------------------------------------------------
+// DSL & Demo
+// --------------------------------------------------------------------------------------
+
+type H() = 
+  static member (?) (_, s:string) = fun attrs children ->
+    let children = children |> List.mapi (fun i -> function 
+      | Element(attrs, c) -> Element(attrs.Add("index", string i), c)
+      | e -> e)
+    Element(Map.ofList(("tag", s)::attrs), children)
+let h = H()
+let txt s = Text(s)
+
+let toHtml (f:string) el = 
+  use wr = new System.IO.StreamWriter(f)
+  let rec loop el = 
+    match el with 
+    | Text s -> wr.Write(s)
+    | Element(attrs, children) ->
+        let other = 
+          attrs.Remove("tag").Remove("index") 
+          |> Seq.map (fun (KeyValue(k, v)) -> $" {k}='{v}'")
+          |> String.concat ""
+        wr.Write($"""<{attrs.["tag"]}{other}>""")
+        for el in children do loop el
+        wr.Write($"""</{attrs.["tag"]}>""")
+  loop el
+
 let doc = 
-  [ Heading(1, [ Text "Programming conference 2023" ]) 
-    Heading(2, [ Text "List of people to invite"])
-    List [
-      [ Paragraph [ Text "Adele Goldberg, adele@xerox.com" ] ]
-      [ Paragraph [ Text "Margaret Hamilton, hamilton@mit.com" ] ]
-      [ Paragraph [ Text "Betty Jean Jennings, betty@rand.com" ] ]
+  h?body [] [
+    h?h1 [] [ txt "Programming conference 2023" ]
+    h?h2 [] [ txt "List of people to invite" ]
+    h?ul [] [
+      h?li [] [ txt "Adele Goldberg, adele@xerox.com" ] 
+      h?li [] [ txt "Margaret Hamilton, hamilton@mit.com" ] 
+      h?li [] [ txt "Betty Jean Jennings, betty@rand.com" ] 
     ]
   ]
 
-let edits1 = [ 
-  MakeTable([Nth [2]], [ Nth [2]; Children; Children ])
-  SplitColumn([Nth [2]], "Column", ["Name"; "Email"], fun (Text s) -> 
-    let i = s.IndexOf(',') in [Text(s.Substring(0, i).Trim()); Text(s.Substring(i+1).Trim())]
-  )
-  SplitColumn([Nth [2]], "Name", ["First"; "Last"], fun (Text s) -> 
-    let i = s.IndexOf(' ') in [Text(s.Substring(0, i).Trim()); Text(s.Substring(i+1).Trim())]
-  )
+let people = [ Any; Tag("ul"); Tag("li") ]
+let jean = [ Any; Tag("ul"); Index(2) ]
+
+doc |> select people
+doc |> select jean
+
+let edits1 = [
+  SetAttr([Any; Tag("ul")], ("tag", "table"))
+  SetAttr([Any; Tag("table"); Tag("li") ], ("tag", "td"))
+  Nest([Any; Tag("table"); Tag("td") ], h?tr [])
+  Duplicate([Any; Tag("table"); Tag("tr"); Tag("td")])
+  EditText([Any; Tag("table"); Tag("tr"); Index(0)], fun s -> s.Substring(0, s.IndexOf(',')))
+  EditText([Any; Tag("table"); Tag("tr"); Index(1)], fun s -> s.Substring(s.IndexOf(',')+1))
+  SetAttr([Any; Tag("table"); Tag("tr"); Tag("td") ], ("style", "border:solid 1px black"))
 ]
 
-let edits2 = [
-  EditElement([Nth [2]; Children; Nth [2]; Children], fun (Text s) ->
-    Text(s.Replace("Betty Jean Jennings", "Jean Jennings Bartik"))
-  )
-]
-
-doc 
+doc
 |> apply edits1.[0]
 |> apply edits1.[1]
 |> apply edits1.[2]
+|> apply edits1.[3]
+|> apply edits1.[4]
+|> apply edits1.[5]
+|> apply edits1.[6]
+|> toHtml "c:/temp/demo.html"
 
-doc
+let edits2 = [
+  EditText([Any; Tag("ul"); Index(2)], fun s -> 
+    s.Replace("Betty Jean Jennings", "Jean Jennings Bartik"))
+]
+
+doc 
 |> apply edits2.[0]
-
-let elementSel = [Nth [2]; Children; Nth [2]; Children]
-
-select elementSel doc
-replace elementSel [Text "!!!"] doc
